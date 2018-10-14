bb139443d719d8536c99ef5e8f4e3d55b5738353
Robin Tram <robin.tram@gmail.com>
2018-10-14 18:18:35 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -6,11 +6,11 @@ class Student
   end 
   
   def hello
-    puts ''
+    puts 'Hey there! I'm so excited to learn stuff.'
   end 
   
   def raise_hand
-    puts 'Pick me!'
+    puts "Pick me!
   end 
   
 end 
