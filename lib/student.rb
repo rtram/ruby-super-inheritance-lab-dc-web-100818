d0fd313a06b5a3ccd98e161f3af6321d907770c0
Robin Tram <robin.tram@gmail.com>
2018-10-14 18:22:33 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,9 +1,16 @@
-
+require 'pry'
 
 class Student 
   
   def initialize
-    
+  end 
+  
+  def hello
+    puts "Hey there! I'm so excited to learn stuff."
+  end 
+  
+  def raise_hand
+    puts "Pick me!"
   end 
   
 end 
